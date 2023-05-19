47c7bcebb2559928cf05f5226ca7aef6f0024e74
Abhinav Verma <abhinavv218@gmail.com>
2023-05-19 08:39:33 +0530

    MAD lab ws

--- Experiment09/app/src/main/java/com/example/experiment09/MainActivity.java
+++ Experiment09/app/src/main/java/com/example/experiment09/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.experiment09;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_LONG).show();
         int id = item.getItemId();
         if (id == R.id.search_item) {
             return true;
